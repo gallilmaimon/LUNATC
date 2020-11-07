b222d7ce7c106f32d3058e32993ce9d92bf46219
gallilmaimon <gallil.maimon97@gmail.com>
2020-11-07 21:27:38 +0200

    WIP: add continuous dqn and add lm batch inference

--- src/TextModels/text_model_utils.py
+++ src/TextModels/text_model_utils.py
@@ -1,5 +1,19 @@
 import torch
 import numpy as np
+from collections import defaultdict
+
+
+def embed_texts(texts, model, tokeniser, num_hiddens=4, maxlen=128, device="cuda"):
+    inputs = tokeniser(texts, padding=True, truncation=True, max_length=maxlen, pad_to_multiple_of=maxlen,
+                       return_tensors='pt')
+
+    inputs['input_ids'] = inputs['input_ids'].to(device)
+    inputs['attention_mask'] = inputs['attention_mask'].to(device)
+    inputs['token_type_ids'] = inputs['token_type_ids'].to(device)
+    
+    with torch.no_grad():
+        outputs = model(**inputs)
+    return torch.cat([layer.unsqueeze(0) for layer in outputs[2][-num_hiddens:]]).mean(dim=[0, 2])
 
 
 def get_bert_hidden_layers(sent, model, tokenizer, device, max_size=512):
@@ -73,6 +87,18 @@ def load_embedding(path):
                 words.append(parts[0])
                 vals += [float(x) for x in parts[1:]]
     return words, np.asarray(vals).reshape(len(words), -1)
+
+
+def load_embedding_dict(path, default_vec):
+    word2vec = defaultdict(lambda: default_vec)
+    with open(path, encoding='utf-8') as fin:
+        fin.readline()
+        for line in fin:
+            line = line.rstrip()
+            if line:
+                parts = line.split(' ')
+                word2vec[parts[0]] = torch.tensor([[float(x) for x in parts[1:]]])
+    return word2vec
 
 
 def glove_tokenify(text, word2ind, pad_ind, max_len=128):
